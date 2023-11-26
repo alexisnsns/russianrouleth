2d53bc45484469e97b3b5f46e2332f6016333eca
alexisnsns <alexisns@pm.me>
2023-11-26 17:24:08 -0500

    v0 rouleth contract

--- contracts/rouleth.vy
+++ contracts/rouleth.vy
@@ -1,4 +1,67 @@
+owner: public(address)
+players: address[6]
+numPlayers: public(uint256)
+totalBalance: public(uint256)
+casinoBalance: public(uint256)
+locked: public(bool)
+
+
 @external
 @view
-def greet() -> String[12]:
-    return "1"
+def greet() -> String[11]:
+    return "hello, anon"
+
+@external
+@payable
+def play():
+    assert not self.locked, "Reentrancy protection"
+    assert msg.value == 0.01 * 10**18, "Send 0.01 ETH"
+
+    self.locked = True
+
+    # Add player to the list
+    self.players[self.numPlayers] = msg.sender
+    self.numPlayers += 1
+
+    # Increment total balance
+    self.totalBalance += msg.value
+
+    # Check if there are 6 players
+    if self.numPlayers == 6:
+        # Pick a random winner
+        winnerIndex: uint256 = convert(sha256(block.timestamp), uint256) % 6
+        winner: address = self.players[winnerIndex]
+
+        # Calculate payout (95% of total balance)
+        payout: uint256 = self.totalBalance * 95 / 100
+
+        # Calculate casino earnings and add to casino balance
+        casinoEarnings: uint256 = self.totalBalance - payout
+        self.casinoBalance += casinoEarnings
+
+        # Reset for next round
+        self.numPlayers = 0
+        self.totalBalance = 0
+
+        self.locked = False
+
+        # Send payout to the winner
+        send(winner, payout)
+    else:
+        self.locked = False
+
+@external
+def withdrawCasinoFunds():
+    assert msg.sender == self.owner, "Only owner can withdraw"
+    self.locked = True
+    send(self.owner, self.casinoBalance)
+    self.casinoBalance = 0
+    self.locked = False
+
+@external
+def __init__():
+    self.owner = msg.sender
+    self.numPlayers = 0
+    self.totalBalance = 0
+    self.casinoBalance = 0
+    self.locked = False
--- test/test.js
+++ test/test.js
@@ -1,13 +1,38 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("SimpleContract", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const SimpleContract = await ethers.getContractFactory("rouleth");
-    const simpleContract = await SimpleContract.deploy();
-    await simpleContract.deployed();
+describe("Rouleth Contract", function () {
+  let Rouleth, rouleth, owner, addr1, addr2, addrs;
 
-    const greeting = await simpleContract.greet();
-    console.log("greeting", greeting);
-    expect(greeting).to.equal("1");
+  beforeEach(async function () {
+    Rouleth = await ethers.getContractFactory("Rouleth");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    rouleth = await Rouleth.deploy();
+    await rouleth.deployed();
+  });
+
+  it("Should receive 0.01 ETH correctly", async function () {
+    const tx = { value: ethers.utils.parseEther("0.01") };
+    await rouleth.connect(addr1).play(tx);
+    expect(await ethers.provider.getBalance(rouleth.address)).to.equal(
+      ethers.utils.parseEther("0.01")
+    );
+  });
+
+  it("Should pay out correctly after 6 transactions", async function () {
+    const tx = { value: ethers.utils.parseEther("0.01") };
+    for (let i = 0; i < 6; i++) {
+      await rouleth.connect(addrs[i]).play(tx);
+    }
+    // Check balance of the contract after 6 transactions
+    const contractBalance = await ethers.provider.getBalance(rouleth.address);
+    expect(contractBalance).to.be.lessThan(ethers.utils.parseEther("0.06"));
+  });
+
+  it("Should allow admin to withdraw casino funds", async function () {
+    const tx = { value: ethers.utils.parseEther("0.01") };
+    await rouleth.connect(addr1).play(tx);
+    await rouleth.connect(owner).withdrawCasinoFunds();
+    expect(await ethers.provider.getBalance(rouleth.address)).to.equal(0);
   });
 });
