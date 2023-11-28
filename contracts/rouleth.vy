b67d383d11963fff5918f4bb83c4f9bb7b8365f5
alexisnsns <alexisns@pm.me>
2023-11-27 21:43:35 -0500

    test and deploy

--- contracts/rouleth.vy
+++ contracts/rouleth.vy
@@ -15,48 +15,53 @@ def greet() -> String[11]:
 @payable
 def play():
     assert not self.locked, "Reentrancy protection"
-    assert msg.value == 0.01 * 10**18, "Send 0.01 ETH"
 
-    self.locked = True
+    # Check if the correct amount is sent
+    if msg.value == 10**16:
+      self.locked = True
 
-    # Add player to the list
-    self.players[self.numPlayers] = msg.sender
-    self.numPlayers += 1
+      # Add player to the list
+      self.players[self.numPlayers] = msg.sender
+      self.numPlayers += 1
 
-    # Increment total balance
-    self.totalBalance += msg.value
+      # Increment total balance
+      self.totalBalance += msg.value
 
-    # Check if there are 6 players
-    if self.numPlayers == 6:
+      # Check if there are 6 players
+      if self.numPlayers == 6:
         # Pick a random winner
-        winnerIndex: uint256 = convert(sha256(block.timestamp), uint256) % 6
+        timestamp_bytes: bytes32 = convert(block.timestamp, bytes32)
+        hash: bytes32 = sha256(timestamp_bytes)
+        winnerIndex: uint256 = convert(hash, uint256) % 6
         winner: address = self.players[winnerIndex]
 
         # Calculate payout (95% of total balance)
         payout: uint256 = self.totalBalance * 95 / 100
 
-        # Calculate casino earnings and add to casino balance
+        # Send payout to the winner
+        send(winner, payout)
+
+        # Calculate casino earnings and send to owner
         casinoEarnings: uint256 = self.totalBalance - payout
-        self.casinoBalance += casinoEarnings
+        send(self.owner, casinoEarnings)
 
         # Reset for next round
         self.numPlayers = 0
         self.totalBalance = 0
 
         self.locked = False
-
-        # Send payout to the winner
-        send(winner, payout)
+      else:
+          self.locked = False
     else:
-        self.locked = False
+      # If the incorrect amount is sent, send it to the owner
+      send(self.owner, msg.value)
 
 @external
-def withdrawCasinoFunds():
-    assert msg.sender == self.owner, "Only owner can withdraw"
-    self.locked = True
-    send(self.owner, self.casinoBalance)
-    self.casinoBalance = 0
-    self.locked = False
+@payable
+def __default__():
+    # Send the received Ether to the owner
+    send(self.owner, msg.value)
+
 
 @external
 def __init__():
--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ describe("Rouleth Contract", function () {
   let Rouleth, rouleth, owner, addr1, addr2, addrs;
 
   beforeEach(async function () {
-    Rouleth = await ethers.getContractFactory("Rouleth");
+    Rouleth = await ethers.getContractFactory("rouleth");
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
     rouleth = await Rouleth.deploy();
     await rouleth.deployed();
@@ -13,26 +13,105 @@ describe("Rouleth Contract", function () {
 
   it("Should receive 0.01 ETH correctly", async function () {
     const tx = { value: ethers.utils.parseEther("0.01") };
-    await rouleth.connect(addr1).play(tx);
-    expect(await ethers.provider.getBalance(rouleth.address)).to.equal(
-      ethers.utils.parseEther("0.01")
+    const receipt = await rouleth.connect(addr1).play(tx);
+    const contractBalance = await ethers.provider.getBalance(rouleth.address);
+    expect(contractBalance.toString()).to.equal(
+      ethers.utils.parseEther("0.01").toString()
     );
   });
 
-  it("Should pay out correctly after 6 transactions", async function () {
-    const tx = { value: ethers.utils.parseEther("0.01") };
-    for (let i = 0; i < 6; i++) {
-      await rouleth.connect(addrs[i]).play(tx);
-    }
-    // Check balance of the contract after 6 transactions
+  it("Should refuse amounts different to 0.01 eth and send them back to the owner", async function () {
+    const incorrectAmount = ethers.utils.parseEther("0.02");
+    const tx = { value: incorrectAmount };
+    const initialOwnerBalance = await ethers.provider.getBalance(owner.address);
+
+    // Execute the transaction
+    const txResponse = await rouleth.connect(addr1).play(tx);
+    await txResponse.wait(); // Wait for the transaction to be mined
+
+    const finalOwnerBalance = await ethers.provider.getBalance(owner.address);
     const contractBalance = await ethers.provider.getBalance(rouleth.address);
-    expect(contractBalance).to.be.lessThan(ethers.utils.parseEther("0.06"));
+
+    // Check if the contract balance is unchanged
+    expect(contractBalance.toString()).to.equal(
+      ethers.utils.parseEther("0.00").toString()
+    );
+
+    // Calculate the expected owner balance after receiving the incorrect amount
+    // Note: This does not account for gas fees paid by the owner for receiving Ether
+    const expectedFinalOwnerBalance = initialOwnerBalance.add(incorrectAmount);
+
+    // Since we can't easily calculate the exact transaction fee, we check if the final balance
+    // is less than or equal to the expected balance
+    expect(finalOwnerBalance.lte(expectedFinalOwnerBalance)).to.be.true;
   });
 
-  it("Should allow admin to withdraw casino funds", async function () {
-    const tx = { value: ethers.utils.parseEther("0.01") };
-    await rouleth.connect(addr1).play(tx);
-    await rouleth.connect(owner).withdrawCasinoFunds();
-    expect(await ethers.provider.getBalance(rouleth.address)).to.equal(0);
+  it("should ensure a player wins the game and receives 95% of the money", async function () {
+    const playValue = ethers.utils.parseEther("0.01");
+    const totalBetAmount = playValue.mul(6);
+    const expectedPayout = totalBetAmount.mul(95).div(100); // 95% of total bets
+
+    // Track initial balances of the players
+    let initialBalances = [];
+    for (let i = 0; i < 6; i++) {
+      initialBalances.push(await ethers.provider.getBalance(addrs[i].address));
+    }
+
+    // Simulate six plays
+    for (let i = 0; i < 6; i++) {
+      await rouleth.connect(addrs[i]).play({ value: playValue });
+    }
+
+    let finalBalances = [];
+
+    for (let i = 0; i < 6; i++) {
+      finalBalances.push(await ethers.provider.getBalance(addrs[i].address));
+    }
+
+    let sum = finalBalances.reduce(
+      (accumulator, currentValue) => accumulator.add(currentValue),
+      ethers.BigNumber.from(0)
+    );
+    console.log("sum", sum);
+
+    let begin = initialBalances.reduce(
+      (accumulator, currentValue) => accumulator.add(currentValue),
+      ethers.BigNumber.from(0)
+    );
+    console.log("begin", begin);
+
+    const result = (begin - sum).toString()
+    let valueInWei = ethers.BigNumber.from(result); // This is 1 ETH in Wei
+
+    // Convert it to Ether
+    let valueInEth = ethers.utils.formatEther(valueInWei);
+    console.log(valueInEth); // Output will be a string "1.0"
+
+    expect(valueInEth).to.equal(expectedPayout)
+  });
+
+  it("Should transfer 5% of the amount to the owner after six plays", async function () {
+    const playValue = ethers.utils.parseEther("0.01");
+    const expectedEarnings = playValue.mul(6).div(20); // 5% of the total amount after six plays
+
+    // Get owner's balance before playing
+    const initialOwnerBalance = await ethers.provider.getBalance(owner.address);
+    // console.log('initial owner balance', initialOwnerBalance)
+
+    // Simulate six plays
+    for (let i = 0; i < 6; i++) {
+      await rouleth.connect(addrs[i]).play({ value: playValue });
+    }
+
+    // Get owner's balance after playing
+    const finalOwnerBalance = await ethers.provider.getBalance(owner.address);
+    // console.log('final owner balance', finalOwnerBalance)
+
+    // Calculate the actual earnings received by the owner
+    const actualEarnings = finalOwnerBalance.sub(initialOwnerBalance);
+    // console.log('earnings', actualEarnings)
+
+    // Check if the owner's earnings are correct
+    expect(actualEarnings.toString()).to.equal(expectedEarnings.toString());
   });
 });
