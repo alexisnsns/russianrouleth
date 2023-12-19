5cb2f1907747434bb374445b2c90eae3b260b668
alexisnsns <alexisns@pm.me>
2023-12-19 17:55:16 -0500

    navigate on wrong url

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import TestRouleth from "./TestRouleth";
 import Rouleth from "./Rouleth";
 
 const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Rouleth />} />
-          <Route path="/test-rouleth" element={<TestRouleth />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Rouleth />} />
+        <Route path="/test-rouleth" element={<TestRouleth />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </Router>
   );
 };
